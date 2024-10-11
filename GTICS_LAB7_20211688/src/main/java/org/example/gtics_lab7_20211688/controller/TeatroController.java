ffade78cfeb7840ccb58e619ce49b7cf0a71223c
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-10-10 20:46:59 -0500

    avance

--- GTICS_LAB7_20211688/src/main/java/org/example/gtics_lab7_20211688/controller/TeatroController.java
+++ GTICS_LAB7_20211688/src/main/java/org/example/gtics_lab7_20211688/controller/TeatroController.java
@@ -2,6 +2,9 @@ package org.example.gtics_lab7_20211688.controller;
 
 
 import org.example.gtics_lab7_20211688.repository.FuncionesRepository;
+import org.example.gtics_lab7_20211688.repository.ObrasRepository;
+import org.example.gtics_lab7_20211688.repository.RoomSeatsRepository;
+import org.example.gtics_lab7_20211688.repository.RoomsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +15,30 @@ public class TeatroController {
     @Autowired
     FuncionesRepository funcionesRepository;
 
+    @Autowired
+    RoomsRepository salasRepository;
+
+    @Autowired
+    ObrasRepository obrasRepository;
 
     @GetMapping({"/Admin/listaFunciones","/Admin"})
     public String funcionesAdmin(Model model){
 
         model.addAttribute("listaFunciones",funcionesRepository.findAll());
         return "listaFunciones";
+    }
+
+    @GetMapping({"/Gerente/listaSalas","/Gerente"})
+    public String salasGerente(Model model){
+
+        model.addAttribute("listaSalas",salasRepository.findAll());
+        return "listaSalas";
+    }
+
+    @GetMapping({"/Gerente/listaObras"})
+    public String obrasGerente(Model model){
+
+        model.addAttribute("listaObras",obrasRepository.findAll());
+        return "listaObras";
     }
 }
