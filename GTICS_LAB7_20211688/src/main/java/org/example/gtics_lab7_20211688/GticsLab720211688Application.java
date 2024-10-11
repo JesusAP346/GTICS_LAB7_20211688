561ae37bb891b7ee3f16ebd4943cc15b4bcd92ee
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-10-10 19:16:19 -0500

    primer commit

---
+++ GTICS_LAB7_20211688/src/main/java/org/example/gtics_lab7_20211688/GticsLab720211688Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab7_20211688;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab720211688Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab720211688Application.class, args);
+    }
+
+}
---
+++ GTICS_LAB7_20211688/src/test/java/org/example/gtics_lab7_20211688/GticsLab720211688ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab7_20211688;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab720211688ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
